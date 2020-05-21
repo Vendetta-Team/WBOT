c9b991bd28115b44946d30e5cd9439669013207b
Vendetta <34066247+vendettadelsangue@users.noreply.github.com>
2020-05-21 14:33:23 +0900

    Delete stop.js

--- Commands/Music/stop.js
+++
@@ -1,9 +0,0 @@
-exports.run = async (client, message, args, lang, me, log, autorole, locale, langage, memo, datas) => {
-    if (!message.guild.me.voice.channel) return message.reply(locale.commands.find(e => e.name.includes('stop'))[lang].message1)
-    if (!message.member.voice.channel) return message.reply(locale.commands.find(e => e.name.includes('stop'))[lang].message2.replace('$[채널]',message.guild.me.voice.channel))
-    if (message.guild.me.voice.channel.id !== message.member.voice.channel.id) return message.reply(locale.commands.find(e => e.name.includes('stop'))[lang].message3.replace('$[채널1]',message.member.voice.channel).replace('$[채널2]',message.guild.me.voice.channel))
-    if (!message.member.permissions.has('ADMINSTRATOR')) return message.reply('❌')
-    message.guild.me.voice.channel.leave();
-    datas.active.delete(dispatcher.guildID)
-    message.reply(locale.commands.find(e => e.name.includes('stop'))[lang].message4)
-}
